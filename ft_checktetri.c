4df209dc6f75494ec33d74ea29d9f7512747fbe0
Natansab <natansab@gmail.com>
2016-11-16 16:55:03 +0100

    tetri

---
+++ ft_checktetri.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_checktetri.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/16 14:49:42 by nsabbah           #+#    #+#             */
+/*   Updated: 2016/11/16 16:54:20 by nsabbah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int ft_checktetri(char *str)
+{
+  int i;
+  int j;
+  int count;
+
+  count = 1;
+  j = 0;
+  i = 0;
+  while (str[j] != '#')
+    j++;
+  i = j + 1;
+  while (str[i])
+  {
+    if (str[i] == '#')
+    {
+      count++;
+      if (str[i - 1] != '#' && str[i - 5] != '#')
+          return (0);
+    }
+    i++;
+  }
+  if (count != 4)
+    return (0);
+  return (1);
+}
+
+/*
+Example of valid grids
+....\n....\n....\n....\n
+..#.\n..#.\n..#.\n..#.\n
+
+Invalid grids
+....\n....\n....\n...\n.
+....\n....\n....\n.....\n
+*/

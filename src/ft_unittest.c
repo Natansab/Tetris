512a6dc5b8fa6bffb82fec1f6a161fd9c281ddba
Emil Wallner <emilw@llner.co>
2016-11-17 22:21:31 +0100

    unittest

--- src/ft_unittest.c
+++ src/ft_unittest.c
@@ -6,7 +6,7 @@
 /*   By: ewallner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 14:41:40 by ewallner          #+#    #+#             */
-/*   Updated: 2016/11/17 21:19:22 by ewallner         ###   ########.fr       */
+/*   Updated: 2016/11/17 21:23:39 by ewallner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 int			main(int argc, char **argv)
 {
 	(void)argc;
-	printf("Check the Grid: %d \n", ft_checkgrid(ft_filetostr(argv[1])));
-	printf("Check the Tetri: %d \n", ft_checktetri(ft_filetostr(argv[1])));
+//	printf("Check the Grid: %d \n", ft_checkgrid(ft_filetostr(argv[1])));
+//	printf("Check the Tetri: %d \n", ft_checktetri(ft_filetostr(argv[1])));
+	puts(ft_filetostr(argv[1]));
 	return (0);
 }

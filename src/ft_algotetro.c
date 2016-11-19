ad1a406eb9720f61c72cdfaee39622b785b5018e
Natansab <natansab@gmail.com>
2016-11-19 14:41:57 +0100

    natan

--- src/ft_algotetro.c
+++ src/ft_algotetro.c
@@ -6,50 +6,100 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 16:48:38 by nsabbah           #+#    #+#             */
-/*   Updated: 2016/11/18 16:57:54 by nsabbah          ###   ########.fr       */
+/*   Updated: 2016/11/19 14:41:06 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*Function to create the string with the format ....\n....\n....\n....\n
-The function has in argument that size of the square. For the example above
-the size should be 4.*/
 
-char *ft_tetrogrid(int i)
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* This function will print the AAAA or BBBB if we need to fit the tetro in
+the grid. If we need to erase the AAAA, erase = 1*/
+
+void ft_printtetro(int *tab, char *grid, int position, int index, int erase)
 {
-  int j;
-  char *str;
+  int i;
 
-  str = (char **)malloc(sizeof(char) * (i * i + i + 1));
-  if (str == NULL)
-    return (NULL);
-  j = 0;
-  while (str[j])
+  i = 0;
+  while (i < 4)
   {
-    if (j % i != (i + 1))
-      str[j] = '.';
+    if (erase)
+      grid[tab[i] + position] = '.';
     else
-      str[j] = '\n';
-    j++;
+      grid[tab[i] + position] = 'A' + index;
+    i++;
   }
-  str[j] = '\0';
-  return (str);
 }
 
-char *ft_algotetro(void)
+/* ft_tetrofits is a function to determine if the tetro will fit in this
+position or if we should change position
+tab = tetro[index]
+*/
+
+int ft_tetrofits(int *tab, char *grid, int position)
 {
-  int tab[4][4];
+  int i;
 
-  /*Creating two tetrominos that are in 4x4 square with newline*/
+  i = 0;
+  while (i < 4)
+  {
+    if (grid[tab[i] + position] != '.')
+      return (0);
+    i++;
+  }
+  return (1);
+}
 
-  tab[0][0] = 0;
-  tab[0][1] = 1;
-  tab[0][2] = 2;
-  tab[0][3] = 6;
+int ft_algotetro(int **tetro, int i, char *grid)
+{
+  int position;
+  int print;
 
-  tab[1][0] = 0;
-  tab[1][1] = 1;
-  tab[1][2] = 2;
-  tab[1][3] = 6;
+// i = index of Tetro
+// Exit statement: when all the Tetros fit in the square
+  if (i > 2/*# of Tetro*/)
+    return (1);
 
+// Always try to fit the block from the first position in the upper left
+position = 0;
+// While position is less that the lenth of the grid we try to fit the tetro
+while (position < strlen(grid))
+{
+  print = 0;
+  if (ft_tetrofits(tetro[i], grid, position))
+  {
+    print = 1;
+    ft_printtetro(tetro[i], grid, position, i, 0);
+    // shoudl pass grid to know the size of the grid
+    if (ft_algotetro(tetro, (i + 1), grid))
+      return (1);
+  }
+/* If we used ft_printtetro before but will change the position, we should
+erase what we did*/
+  if (print)
+    ft_printtetro(tetro[i], grid, position, i, 1);
+  position ++;
+}
 
+/* If tetro 0 is out the grid make a bigger grid. Put aside for now but
+we should use a ft_tetroconv with the new size of the grid*/
+  /*if (i = 0)
+    return (ft_algotetro(tetro[0] grid++)); */
+return (0);
+}
+
+int main(void)
+{
+  printf("hello");
+  int tetro[3][4] =
+  {
+    {0, 1, 5, 6},
+    {0, 1, 5, 6},
+    {0, 1, 5, 6}
+  };
+  printf("hello");
+  char grid[50] = "....\n....\n....\n....\n";
+  return(ft_algotetro((int **)tetro, 0, grid));
 }
